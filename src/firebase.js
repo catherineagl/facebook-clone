cf682da40fb8dfc803645cbfa0ffa21ae678f27e
Catherine Aguilera <catherineaguilera13@gmail.com>
2022-03-16 16:24:37 -0400

    add firebase cnfig

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import {
+	getAuth,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+} from 'firebase/auth';
+import { arrayUnion, getFirestore, updateDoc } from 'firebase/firestore';
+import { doc, setDoc, addDoc } from 'firebase/firestore';
 const firebaseConfig = {
 	apiKey: 'AIzaSyAWHTyZf-iPi31MpUAjF4rlTLd-5zr8lF4',
 	authDomain: 'facebook-clone-df941.firebaseapp.com',
@@ -13,5 +17,81 @@ const firebaseConfig = {
 	appId: '1:678086236440:web:38018515a0cdbc324ae66d',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+export const createUser = (form) => {
+	createUserWithEmailAndPassword(auth, form.email, form.password)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			if (user) {
+				let birthday = new Date([
+					form.birthYear,
+					form.birthMonth,
+					form.birthDay,
+				]);
+
+				//save data in collection
+				const userData = doc(db, 'users', user.uid, 'data');
+				const data = {
+					name: form.name,
+					surname: form.surname,
+					email: form.email,
+					birthday: birthday,
+					password: form.password,
+					sex: form.sex,
+					pronoun: form.pronoun,
+					custom: form.custom,
+					friends: [],
+					posts: [],
+				};
+				setDoc(userData, data);
+			}
+			// ...
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			alert(`Error ${errorCode}: ${errorMessage}`);
+			// ..
+		});
+};
+
+export const signIn = (email, password) => {
+	const auth = getAuth();
+	signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			console.log(user);
+
+			// ...
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			alert(`Error ${errorCode}: ${errorMessage}`);
+		});
+};
+
+export const checkUser = () => {
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			const uid = user.uid;
+
+			console.log(uid);
+		} else {
+		}
+	});
+};
+
+export const createPost = (data) => {
+	const userPosts = doc(db, 'posts', 'DzrBdztD4rOkq97nepVT9EcFfgc2', data.id);
+	setDoc(userPosts, data);
+	/* const userData = doc(db, 'users', 'DzrBdztD4rOkq97nepVT9EcFfgc2');
+	updateDoc(userData, { posts: arrayUnion(data.uid) }); */
+	console.log('update suscessfull');
+};
+// Initialize Firebase
